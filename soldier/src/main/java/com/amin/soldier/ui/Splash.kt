9beabfe762b65c6f87fcd4a86ee50609cd515104
Amin <17686403580@163.com>
2023-12-22 11:04:08 +0800

    权限申请1

--- soldier/src/main/java/com/amin/soldier/ui/Splash.kt
+++ soldier/src/main/java/com/amin/soldier/ui/Splash.kt
@@ -6,34 +6,12 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
@@ -42,8 +20,6 @@ import com.amin.soldier.utils.DialogTwoBtn
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.winspread.solidierlib.utils.loge
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
@@ -115,13 +91,14 @@ fun permissionRequest(
         true -> {
             success.invoke(true)
         }
+
         false -> {
             var textToShow = ""
             var textToTip = ""
             var textToGo = ""
             resumeShouldShowRationale = multiplePermissionsState.shouldShowRationale
 
-            if (resumeShouldShowRationale&&!first) {
+            if (resumeShouldShowRationale && !first) {
                 textToShow = firstContent
                 textToTip = "退出"
                 textToGo = "去授权"
@@ -140,8 +117,6 @@ fun permissionRequest(
                 }
                 return
             }
-
-                .
 
             DialogTwoBtn(textToShow, textToTip, textToGo, {
                 android.os.Process.killProcess(android.os.Process.myPid())
